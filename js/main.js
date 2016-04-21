c119b2fea9f0acde2ed5227b94ea10a03104d524
KatherineB <kbiefer@golden.net>
2016-04-21 17:43:08 -0400

    added some javascript, fixed other minor things

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-// Your code here ...
+var otherActivity = 0;
+
+function changePick(pick){
+	switch(pick)
+	{
+		case 1: document.getElementById("pick_1").style.opacity = "1";
+                document.getElementById("pick_2").style.opacity = "0";
+				document.getElementById("pick_3").style.opacity = "0";
+				document.getElementById("top-sub-text-1").style.display = "inline-block";
+                document.getElementById("top-sub-text-2").style.display = "none";
+				document.getElementById("top-sub-text-3").style.display = "none";
+				break;
+				
+		case 2: document.getElementById("pick_1").style.opacity = "0";
+                document.getElementById("pick_2").style.opacity = "1";
+				document.getElementById("pick_3").style.opacity = "0";
+				document.getElementById("top-sub-text-1").style.display = "none";
+                document.getElementById("top-sub-text-2").style.display = "inline-block";
+				document.getElementById("top-sub-text-3").style.display = "none";
+				break;
+				
+		case 3: document.getElementById("pick_1").style.opacity = "0";
+                document.getElementById("pick_2").style.opacity = "0";
+				document.getElementById("pick_3").style.opacity = "1";
+				document.getElementById("top-sub-text-1").style.display = "none";
+                document.getElementById("top-sub-text-2").style.display = "none";
+				document.getElementById("top-sub-text-3").style.display = "inline-block";
+				break;
+    }
+}
+
+function toggleOtherActivity(){
+	if(otherActivity == 0){
+		document.getElementById("activity-box-3").style.display = "inline-block";
+		document.getElementById("divider-activity-3").style.display = "inline-block";
+		otherActivity = 1;
+	}
+	else if(otherActivity == 1){
+		document.getElementById("activity-box-3").style.display = "none";
+		document.getElementById("divider-activity-3").style.display = "none";
+		otherActivity = 0;
+	}
+}
